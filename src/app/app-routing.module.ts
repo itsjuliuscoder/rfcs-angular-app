9dbf15df1bdec58f1fcc1408f4facddd50e9034f
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2020-05-01 01:10:41 +0100

    First commit made for this project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './public/index/index.component';
+import { AboutComponent } from './public/about/about.component';
+import { ContactComponent } from './public/contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent, data: {title : 'About US - RFCS'}},
+  { path: 'index', component: IndexComponent, data: {title : 'Technology Driven Education for Every Child - RFCS'}},
+  { path: 'contact', component: ContactComponent, data: {title : 'Contact Us - RFCS'} }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { IndexComponent } from './public/index/index.component';
+import { AboutComponent } from './public/about/about.component';
+import { ContactComponent } from './public/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
